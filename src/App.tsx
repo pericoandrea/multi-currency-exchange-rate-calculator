983ce9152383292e87a233008876421e30b9f191
Andrea Perico <pericoandrea@gmail.com>
2024-11-06 10:40:52 -0500

    Update App.tsx MoneyTransfer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import CurrencyCalculator from './components/CurrencyCalculator';
+import MoneyTransfer from './components/MoneyTransfer';
 
 const queryClient = new QueryClient();
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <div className="min-h-screen bg-gray-100 flex items-center justify-center p-4">
+        <MoneyTransfer />
         <CurrencyCalculator />
       </div>
     </QueryClientProvider>
